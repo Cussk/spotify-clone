ab42ede84b24838243f9367231d8572eecf7a31f
Kyle Cuss <cussprogramming@gmail.com>
2022-10-01 09:46:21 -0230

    Play pause icons for song card implemented
    Play/pause icons imported and logic created.

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,5 +1,21 @@
-const PlayPause = () => (
-  <div>Loader</div>
-);
+import {FaPauseCircle, FaPlayCircle} from 'react-icons/fa';
+
+const PlayPause = ({isPlaying, activeSong, song, handlePause, handlePlay}) => 
+//if song is playing AND the active song === song.title property render pause icon
+(isPlaying && activeSong?.title === song.title ? (
+  <FaPauseCircle 
+    size={35}
+    className='text-gray-300'
+    onClick={handlePause}
+  />
+) : 
+// else render play icon
+(
+  <FaPlayCircle 
+    size={35}
+    className='text-gray-300'
+    onClick={handlePlay}
+  />
+));
 
 export default PlayPause;
